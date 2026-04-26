b14e4655775e89863f6e695ad71e8dee0a22db68
practical-coding-test <real.coding.test.guide@gmail.com>
2026-04-26 22:31:42 +0900

    Update 5_파이프 옮기기2.py

--- python/03장_동적 프로그래밍/5_파이프 옮기기2.py
+++ python/03장_동적 프로그래밍/5_파이프 옮기기2.py
@@ -15,16 +15,16 @@ dj = [1,0,1]
 # 가로는 오른쪽 한 칸만 빈 공간이면 됩니다.
 # 세로는 아래 한 칸만 빈 공간이면 됩니다.
 # 대각선 아래는 오른쪽, 아래 오른쪽 아래 대각선 칸이 빈 공간이어야 합니다.
-checkI = [[0],[1],[0,1,1]]
-checkJ = [[1],[0],[1,0,1]]
+check_i = [[0],[1],[0,1,1]]
+check_j = [[1],[0],[1,0,1]]
 
 # 가로 파이프는 대각선 방향과 가로 방향 파이프를 이어서 놓을 수 있습니다.
 # 세로 파이프는 대각선 방향과 세로 방향 파이프를 이어서 놓을 수 있습니다.
 # 대각선 파이프는 대각선 방향과 세로 방향, 가로 방향 파이프를 모두 놓을 수 있습니다.
-nextPipe = [[0,2], [1,2], [0,1,2]]
+next_pipe = [[0,2], [1,2], [0,1,2]]
 
-def validPipe(house, i, j, p):
-    for di, dj in zip(checkI[p], checkJ[p]):
+def valid_pipe(house, i, j, p):
+    for di, dj in zip(check_i[p], check_j[p]):
         ci = i + di
         cj = j + dj
         if not (0 <= ci < n and 0 <= cj < n) or house[ci][cj] == 1: return False
@@ -37,8 +37,8 @@ for i in range(n):
     for j in range(n):
         if house[i][j] == 1: continue
         for p in range(3):
-            for np in nextPipe[p]:
-                if validPipe(house, i,j, np):
+            for np in next_pipe[p]:
+                if valid_pipe(house, i, j, np):
                     ni = i + di[np]
                     nj = j + dj[np]
                     pipe[np][ni][nj] += pipe[p][i][j]
