8fee9c579a68c6decd992834200b39ea825777d2
practical-coding-test <real.coding.test.guide@gmail.com>
2026-04-26 23:48:13 +0900

    Update 2_타임머신.py

--- python/11장_최단 거리/2_타임머신.py
+++ python/11장_최단 거리/2_타임머신.py
@@ -4,29 +4,29 @@ n, m = map(int, input().split())
 INF = 500 * 6000 * 10000
 
 edges = []
-arrive_time = [INF] * (n + 1)
-arrive_time[1] = 0
+arrival_time = [INF] * (n + 1)
+arrival_time[1] = 0
 
 for _ in range(m):
     a, b, c = map(int, input().split())
     edges.append((a, b, c))
 
-infinity_time_travel = False
+infinite_time_travel = False
 
 for count in range(n):
     for a, b, time in edges:
-        if arrive_time[a] == INF: continue
-        if arrive_time[a] + time < arrive_time[b]:
+        if arrival_time[a] == INF: continue
+        if arrival_time[a] + time < arrival_time[b]:
             if count == n - 1:
-                infinity_time_travel = True
+                infinite_time_travel = True
 
-            arrive_time[b] = arrive_time[a] + time
+            arrival_time[b] = arrival_time[a] + time
 
-if infinity_time_travel:
+if infinite_time_travel:
     print(-1)
 else:
     for city in range(2, n+1):
-        if arrive_time[city] == INF:
+        if arrival_time[city] == INF:
             print(-1)
         else:
-            print(arrive_time[city])
+            print(arrival_time[city])
