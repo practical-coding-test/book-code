a6b4fe4c844520ee0ddf856fa52566e14d5c1f1a
practical-coding-test <real.coding.test.guide@gmail.com>
2026-04-26 21:36:27 +0900

    Update 3_온풍기 안녕.py

--- python/01장_구현(시뮬레이션)/3_온풍기 안녕.py
+++ python/01장_구현(시뮬레이션)/3_온풍기 안녕.py
@@ -9,8 +9,8 @@ dc = [0,1,0,-1]
 
 def is_end(temperature, chocolate, check):
     ok = 0
-    for r,c in check:
-        ok += 1 if temperature[r][c] >= K else 0
+    for r, c in check:
+        ok += 1 if temperature[r][c] >= k else 0
     return ok == len(check) or chocolate > 100
 
 def cold(temperature):
@@ -20,7 +20,7 @@ def cold(temperature):
         if temperature[i][-1]: temperature[i][-1] -= 1
         if temperature[i][0]: temperature[i][0] -= 1
 
-    for j in range(1,m-1):
+    for j in range(1, m-1):
         if temperature[0][j]: temperature[0][j] -= 1
         if temperature[-1][j]: temperature[-1][j] -= 1
 
@@ -33,9 +33,9 @@ def adjust_temp(temperature, wall):
         for c in range(m):
             for d in range(4):
                 if wall[r][c] & (1<<d): continue
-                nr,nc = r+dr[d], c + dc[d]
+                nr, nc = r + dr[d], c + dc[d]
                 if temperature[r][c] > temperature[nr][nc]:
-                    v = (temperature[r][c] - temperature[nr][nc])//4
+                    v = (temperature[r][c] - temperature[nr][nc]) // 4
                     diff[nr][nc] += v
                     diff[r][c] -= v
     
@@ -43,17 +43,17 @@ def adjust_temp(temperature, wall):
         for c in range(m):
             temperature[r][c] += diff[r][c]
 
-def hot_wind(temperature, wall, hitter, visit):
+def hot_wind(temperature, wall, heater, visit):
     global offset
-    offset +=1
-    r,c,d = hitter
+    offset += 1
+    r, c, d = heater
 
-    dq = deque([(r,c,5)])
+    q = deque([(r, c, 5)])
 
     visit[r][c] = offset
 
-    while dq:
-        r,c,t = dq.popleft()
+    while q:
+        r, c, t = q.popleft()
         if t == 0: continue
 
         temperature[r][c] += t
@@ -62,7 +62,7 @@ def hot_wind(temperature, wall, hitter, visit):
             nr, nc = r + dr[d], c + dc[d]
             if visit[nr][nc] != offset:
                 visit[nr][nc] = offset
-                dq.append((nr,nc,t-1))
+                q.append((nr, nc, t-1))
 
         for nextd in [(d+3)%4, (d+1)%4]:
             if wall[r][c] & (1 << nextd): continue
@@ -74,44 +74,46 @@ def hot_wind(temperature, wall, hitter, visit):
             nnc = nc + dc[d]
             if visit[nnr][nnc] != offset:
                 visit[nnr][nnc] = offset
-                dq.append((nnr,nnc,t-1))
+                q.append((nnr, nnc, t-1))
 
-def simulate(temperature, hitter, wall, visit):
-    for hit in hitter:
-        hot_wind(temperature,wall, hit, visit)
+def simulate(temperature, heaters, wall, visit):
+    for heater in heaters:
+        hot_wind(temperature,wall, heater, visit)
     adjust_temp(temperature, wall)
     cold(temperature)
 
-R,C,K = map(int, input().split())
-wall = [[0]*(C) for _ in range(R)]
+n, m, k = map(int, input().split())
+wall = [[0]*(m) for _ in range(n)]
 
-hitter = []
+heaters = []
 check = []
-temperature = [[0]*C for _ in range(R)]
+temperature = [[0]*m for _ in range(n)]
 
 #map init
-for j in range(C):
+for j in range(m):
     wall[0][j] |= 1
     wall[-1][j] |= 4
-for i in range(R):
+for i in range(n):
     wall[i][0] |= 8 
     wall[i][-1] |= 2
 
 # room passing
-room = [list(map(int, input().split())) for _ in range(R)]
-for i in range(R):
-    for j,v in enumerate(room[i]):
+room = [list(map(int, input().split())) for _ in range(n)]
+for i in range(n):
+    for j, v in enumerate(room[i]):
+        # 조사 대상 칸 좌표 수집
         if v == 5:
             check.append((i,j))
+        # 온풍기 좌표 수집
         elif v != 0:
             d = conv[v]
-            hitter.append((i + dr[d],j + dc[d],d))
+            heaters.append((i + dr[d], j + dc[d], d))
 
 # 0: 상 1 : 우
 for i in range(int(input())):
-    r,c,d = map(int,input().split())
-    r-=1
-    c-=1
+    r, c, d = map(int,input().split())
+    r -= 1
+    c -= 1
     if d == 0:
         wall[r][c] |= 1
         wall[r-1][c] |= 4
@@ -119,11 +121,11 @@ for i in range(int(input())):
         wall[r][c] |= 2
         wall[r][c+1] |= 8
 
-visit = [[0]*C for _ in range(R)]
+visit = [[0]*m for _ in range(n)]
 chocolate = 0
 
 while not is_end(temperature, chocolate, check):
-    simulate(temperature, hitter, wall, visit)
+    simulate(temperature, heaters, wall, visit)
     chocolate += 1
 
 print(chocolate)
