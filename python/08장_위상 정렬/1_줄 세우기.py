731fececf425b46cd51b7f2e1b37c3dca9cf7150
practical-coding-test <real.coding.test.guide@gmail.com>
2026-04-26 23:33:41 +0900

    Update 1_줄 세우기.py

--- python/08장_위상 정렬/1_줄 세우기.py
+++ python/08장_위상 정렬/1_줄 세우기.py
@@ -3,9 +3,9 @@
 from collections import deque
 
 n, m = map(int, input().split())
-indegree = [0] * (n + 1)
-adj = [[] for _ in range(n + 1)]
-dq = deque()
+indegree = [0] * (n+1)
+adj = [[] for _ in range(n+1)]
+q = deque()
 
 for i in range(m):
     front, rear = map(int, input().split())
@@ -14,13 +14,13 @@ for i in range(m):
     # 뒷사람의 진입 차수를 증가시킵니다.
     indegree[rear] += 1
 
-for i in range(1, n + 1):
+for i in range(1, n+1):
     # 진입 차수가 0인 노드들을 큐에 삽입합니다.
     if indegree[i] == 0:
-        dq.append(i)
+        q.append(i)
 
-while dq:
-    student = dq.popleft()
+while q:
+    student = q.popleft()
     # 처리된 학생은 바로 출력합니다.
     print(student, end=' ')
 
@@ -29,4 +29,4 @@ while dq:
         # 이후에 앞에 있어야하는 학생이 없다면 처리 가능하므로 큐에 삽입합니다.
         indegree[next] -= 1
         if indegree[next] == 0:
-            dq.append(next)
+            q.append(next)
